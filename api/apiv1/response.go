3662258f46c827e4a16e3c2c04c87bfab9454f0f
Tao Yang <yangtao3601@126.com>
2019-04-23 19:25:58 +0800

    add api response

---
+++ api/apiv1/response.go
@@ -0,0 +1,56 @@
+package apiv1
+
+import (
+    "net/http"
+    "encoding/json"
+)
+
+func WriteSuccessResponse(w http.ResponseWriter, res interface{}) {
+    r, _ := json.Marshal(res)
+    WriteJsonSuccessResponse(w, r)
+}
+
+func WriteJsonSuccessResponse(w http.ResponseWriter, res []byte) {
+    w.WriteHeader(http.StatusOK)
+    w.Write(res)
+}
+
+type ErrorResponse struct {
+    ErrCode     int             `json:"errcode"`
+    ErrMsg      string          `json:"errmsg"`
+}
+
+func ToErrJson(errno int) []byte {
+    response := ErrorResponse{
+        ErrCode:    errno,
+        ErrMsg:     ErrMsg(errno),
+    }
+    r, _ := json.Marshal(response)
+    return r
+}
+
+// 500
+func WriteInternalServerErrorResponse(w http.ResponseWriter, errno int) {
+    r := ToErrJson(errno)
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write(r)
+}
+
+// 400
+func WriteBadRequestResponse(w http.ResponseWriter, errno int) {
+    r := ToErrJson(errno)
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write(r)
+}
+
+// 401
+func WriteUnauthorizedResponse(w http.ResponseWriter) {
+    w.WriteHeader(http.StatusUnauthorized)
+    w.Write([]byte("Unauthorized"))
+}
+
+// 403
+func WriteForbiddenResponse(w http.ResponseWriter) {
+    w.WriteHeader(http.StatusForbidden)
+    w.Write([]byte("Forbidden"))
+}
